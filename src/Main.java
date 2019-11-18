fcb654ad6d2e75ffca2cd8b1f91fa8ba478f62bc
Ivan Kh <i.khalimonchuk@gmail.com>
2019-11-18 13:58:32 +0200

    hm2 commit;

--- src/Main.java
+++ src/Main.java
@@ -15,37 +15,6 @@ public class Main {
         System.out.println(Arrays.toString(ar4.odd()));
         System.out.println(Arrays.toString(ar1.random()));
 
-//test2
 
-/*        Person person = new Person("Alan", 34);
-        Person person2 = new Person(34 , "test");
-        Person person3 = new Person ("test");
-
-        Person pers4 = person2.getPerson();
-        System.out.println(pers4.name);
-
-
-       // int b = person3.ageTo18();
-
-
-        person.name = "John";
-
-        System.out.println(person.name);
-        System.out.println(person.age);
-
-        person.whoAmI();
-
-        ///fff
-
-        int i=0;
-        while (i<10) {
-            i++;
-            if (i%2==0) {
-                System.out.println(i);
-            }
-            else {
-
-            }
-        }*/
     }
 }
